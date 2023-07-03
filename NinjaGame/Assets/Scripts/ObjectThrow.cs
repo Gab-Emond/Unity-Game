9073aed690fb3850b0112d3d39530689242d71c4
Gabriel <emondrpgabriel@gmail.com>
2023-07-03 13:47:09 -0400

    Update ObjectThrow.cs
    Clarification of Ik system organization and hierarchy (to make into seperate classes)

--- NinjaGame/Assets/Scripts/ObjectThrow.cs
+++ NinjaGame/Assets/Scripts/ObjectThrow.cs
@@ -4,13 +4,73 @@ public class ObjectThrow : MonoBehaviour {
     Limb throwingArm;
 
     //Event throw;
+    Transform projectileSpawn;
+    Projectile objectThrown;
 
-    Transform objectThrown;
+    IKController playerIkController;
 
-    void Throw(){
+    void Throw(Vector3 velocity){//thrown velocity 
+        
+        //if was still throwing, end throw 
+        //(or/and set time between throw to always larger than throw)?
+
         if(!throwingArm.Active){
             throwingArm.Activate();
         }
-        throwingArm.Retarget();
+        
+        throwingArm.Retarget(objectThrown.transform);
+        //end throw naturally after time
+        //or end throw once max exten
+
+
+
+        // to find the progression needed to reach projectile before throw
+
+        //knowing projectile direction and limb length+ limb origin, 
+        //by the time or before having reached max point it needs to have reached
+        //Vector3 endPos = Limb.CurrPos+projectileDir;
+        //float timeToReachArmLength= (projectileSpawn.position-throwingArm.CurrPos).magnitude/(2*velocity.magnitude);
+
+        Vector3 relVect = projectileSpawn.position-throwingArm.CurrPos;//dx
+        //Vector3 dv = velocity;
+
+        float timeToReachArmLength = throwingArm.MaxLength*throwingArm.MaxLength*(velocity.sqrMagnitude)/Vector3.Dot(relVect,velocity);
+        float lerpTime=Time.time;
+
+
+        while (lerpTime<timeToReachArmLength)
+        {
+            
+        }
+
+        playerIkController.EndMovement(this);
+        //ik controller script decides and sends start message movement 
+        //stop targetting after, send end movement message (to global ik controller script)
+
+
     }
 }
+
+public class IKController {
+
+    
+    public void EndMovement(IkMovement movement){
+
+    } //end the movement currently playing; also decide if need to start or end next movement
+}
+
+public class Limb {
+    public bool Active;
+    public float MaxLength;
+
+    public Vector3 CurrPos;
+
+    public void Activate(){
+
+    }
+    public void Retarget(Transform _targ){
+
+    }
+
+}
+
